9dcc286b0881578ef4bcd2e586d175aed8695ba1
09_Tina <t8i2n6a14@gmail.com>
2022-08-06 17:51:10 +0800

    feat: 初次Spring Boot上傳

---
+++ src/main/java/com/agjs/hotel/AgjsBootApplication.java
@@ -0,0 +1,13 @@
+package com.agjs.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgjsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgjsBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agjs/hotel/AgjsBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agjs.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgjsBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
